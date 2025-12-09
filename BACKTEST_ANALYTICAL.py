623d8ab7c13ac40cac2674c3982ee0a16bd8e724
jessego20 <jessego@umich.edu>
2025-12-08 23:44:23 -0500

    #####################

--- BACKTEST_ANALYTICAL.py
+++ BACKTEST_ANALYTICAL.py
@@ -177,9 +177,9 @@ class BACKTEST:
         # Symbol to name mapping
         etf_symbol_name_dict = {
             # BOND ETFS
-            'BIL': 'SPDR Bloomberg 1-3 Month T-Bill ETF',
-            'SHY': 'iShares 1-3 Year Treasury Bond ETF',
-            'IEF': 'iShares 7-10 Year Treasury Bond ETF',
+            # 'BIL': 'SPDR Bloomberg 1-3 Month T-Bill ETF',
+            # 'SHY': 'iShares 1-3 Year Treasury Bond ETF',
+            # 'IEF': 'iShares 7-10 Year Treasury Bond ETF',
             
             # MAJOR INDICES
             '^GSPC': 'S&P 500',
@@ -194,17 +194,25 @@ class BACKTEST:
             'USO': 'United States Oil Fund',
             'UNG': 'United States Natural Gas Fund',
             'DBA': 'Invesco DB Agriculture',
+            'CPER': 'United States Copper Index Fund',
+            'DBB': 'Invesco DB Base Metals',
+            'PALL': 'abrdn Palladium Shares ETF',
+            'PLTM': 'GraniteShares Platinum Trust',
+            'WEAT': 'Teucrium Wheat Fund',
+            'SOYB': 'Teucrium Soybean Fund',
+            'CORN': 'Teucrium Corn Fund',
+            'CANE': 'Teucrium Sugar Fund',
             
             # MAIN BROAD MARKET ETFS
             'SPY': 'SPDR S&P 500 ETF',
-            'VOO': 'Vanguard S&P 500 ETF',
-            'RSP': 'Invesco S&P 500 Equal Weight ETF',
-            'IVV': 'iShares Core S&P 500 ETF',
+            # 'VOO': 'Vanguard S&P 500 ETF',
+            # 'RSP': 'Invesco S&P 500 Equal Weight ETF',
+            # 'IVV': 'iShares Core S&P 500 ETF',
             'QQQ': 'Invesco QQQ Trust',
             'IWM': 'iShares Russell 2000 ETF',
-            'IWB': 'iShares Russell 1000 ETF',
+            # 'IWB': 'iShares Russell 1000 ETF',
             'DIA': 'SPDR Dow Jones Industrial Average ETF',
-            'VTI': 'Vanguard Total Stock Market ETF',
+            # 'VTI': 'Vanguard Total Stock Market ETF',
             
             # S&P 500 SECTOR ETFS
             'XLE': 'Energy Select Sector SPDR',
@@ -216,49 +224,44 @@ class BACKTEST:
             'XLB': 'Materials Select Sector SPDR',
             'XLY': 'Consumer Discretionary Select Sector SPDR',
             'XLP': 'Consumer Staples Select Sector SPDR',
-            'XLRE': 'Real Estate Select Sector SPDR',
-            'XLC': 'Communication Services Select Sector SPDR',
-            
+            # 'XLRE': 'Real Estate Select Sector SPDR',
+            # 'XLC': 'Communication Services Select Sector SPDR',
             'IYR': 'iShares U.S. Real Estate ETF',
+            'IYZ': 'iShares U.S. Telecommunications ETF',
             
             # GROWTH ETFs
+            'MGK': 'Vanguard Mega Cap Growth ETF',
             'IVW': 'iShares S&P 500 Growth ETF',
-            'IWF': 'iShares Russell 1000 Growth ETF',
+            # 'IWF': 'iShares Russell 1000 Growth ETF',
             'IWO': 'iShares Russell 2000 Growth ETF',
-            'VUG': 'Vanguard Growth ETF',
+            # 'VUG': 'Vanguard Growth ETF',
             
             # VALUE ETFs
+            'MGV': 'Vanguard Mega Cap Value ETF',
             'IVE': 'iShares S&P 500 Value ETF',
-            'IWD': 'iShares Russell 1000 Value ETF',
+            # 'IWD': 'iShares Russell 1000 Value ETF',
             'IWN': 'iShares Russell 2000 Value ETF',
-            'VTV': 'Vanguard Value ETF',
+            # 'VTV': 'Vanguard Value ETF',
             
             # SIZE ETFs
+            'OEF': 'iShares S&P 100 ETF',
             'IWR': 'iShares Russell Mid-Cap ETF',
             'IWC': 'iShares Micro-Cap ETF',
-            'IJH': 'iShares Core S&P Mid-Cap ETF',
-            'IJR': 'iShares Core S&P Small-Cap ETF',
-            'MDY': 'SPDR S&P MidCap 400 ETF',
+            # 'IJH': 'iShares Core S&P Mid-Cap ETF',
+            # 'IJR': 'iShares Core S&P Small-Cap ETF',
+            # 'MDY': 'SPDR S&P MidCap 400 ETF',
             
             # INTERNATIONAL
-            'VXUS': 'Vanguard Total International Stock ETF',
+            # 'VXUS': 'Vanguard Total International Stock ETF',
             'VEA': 'Vanguard FTSE Developed Markets ETF',
             'VWO': 'Vanguard FTSE Emerging Markets ETF',
             'VGK': 'Vanguard FTSE Europe ETF',
-            'VPL': 'Vanguard FTSE Pacific ETF',
+            # 'VPL': 'Vanguard FTSE Pacific ETF',
             'FXI': 'iShares China Large-Cap ETF',
             'EWJ': 'iShares MSCI Japan ETF',
             'INDA': 'iShares MSCI India ETF',
-            'EFA': 'iShares MSCI EAFE ETF',
+            # 'EFA': 'iShares MSCI EAFE ETF',
             'EEM': 'iShares MSCI Emerging Markets ETF',
-            
-            # BONDS
-            'AGG': 'iShares Core U.S. Aggregate Bond ETF',
-            'BND': 'Vanguard Total Bond Market ETF',
-            'LQD': 'iShares iBoxx $ Investment Grade Corporate Bond ETF',
-            'HYG': 'iShares iBoxx $ High Yield Corporate Bond ETF',
-            'TLT': 'iShares 20+ Year Treasury Bond ETF',
-            'DBC': 'Invesco DB Commodity Index Tracking Fund',
         }
         
         data_path = Path('data/processed/all_etf_data.csv')
